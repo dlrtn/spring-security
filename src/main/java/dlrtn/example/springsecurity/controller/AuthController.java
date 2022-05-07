e7e5c523328ecd21e5e54eba177e1258d3bc5f64
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-08 00:26:17 +0900

    AuthController 수정

--- src/main/java/dlrtn/example/springsecurity/controller/AuthController.java
+++ src/main/java/dlrtn/example/springsecurity/controller/AuthController.java
@@ -2,6 +2,8 @@ package dlrtn.example.springsecurity.controller;
 
 import dlrtn.example.springsecurity.dto.JwtRequestDto;
 import dlrtn.example.springsecurity.dto.MemberSignupRequestDto;
+import dlrtn.example.springsecurity.service.AuthService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@AllArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
 
     @PostMapping(value = "login", produces = MediaType.APPLICATION_JSON_VALUE)
     public String login(@RequestBody JwtRequestDto request) {
-        return "login";
+        try {
+            return authService.login(request);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 
-    @PostMapping(value = "signup", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String signup(@RequestBody MemberSignupRequestDto request) {
-        return "signup";
-    }
 }
