e94667042a2f37c720fe6b7023b15a1471d20b9c
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-08 00:24:36 +0900

    authenticationManager Bean 생성

--- src/main/java/dlrtn/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/dlrtn/example/springsecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package dlrtn.example.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -49,4 +50,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // 정적인 파일 요청에 대해 무시
         web.ignoring().antMatchers(AUTH_WHITELIST);
     }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
