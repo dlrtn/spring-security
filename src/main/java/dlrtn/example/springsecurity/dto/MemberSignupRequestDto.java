7ee68857e54aad4503d981c817f626f0643151d2
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-07 23:31:30 +0900

    Dto 객체 설정

---
+++ src/main/java/dlrtn/example/springsecurity/dto/MemberSignupRequestDto.java
@@ -0,0 +1,14 @@
+package dlrtn.example.springsecurity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberSignupRequestDto {
+
+    private String email;
+    private String password;
+    private String name;
+    
+}
