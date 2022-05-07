24d97bcd934270e5e3f147c6ef67f0202f4ef738
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-08 00:26:05 +0900

    AuthService 수정

---
+++ src/main/java/dlrtn/example/springsecurity/service/AuthService.java
@@ -0,0 +1,50 @@
+package dlrtn.example.springsecurity.service;
+
+import dlrtn.example.springsecurity.domain.Member;
+import dlrtn.example.springsecurity.dto.JwtRequestDto;
+import dlrtn.example.springsecurity.dto.MemberSignupRequestDto;
+import dlrtn.example.springsecurity.repository.MemberRepository;
+import dlrtn.example.springsecurity.security.UserDetailsImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    public String signup(MemberSignupRequestDto request) {
+        boolean existMember = memberRepository.existsById(request.getEmail());
+
+        // 이미 회원이 존재하는 경우
+        if (existMember) return null;
+
+        Member member = new Member(request);
+        member.encryptPassword(passwordEncoder);
+
+        memberRepository.save(member);
+        return member.getEmail();
+    }
+
+    public String login(JwtRequestDto request) throws Exception {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
+        return principal.getUsername();
+    }
+
+}
